e1c8de865c3d3a2aa64c1fab287f0ea4e9d469f2
vespercore02 <willem.leonardo@gmail.com>
2020-02-19 09:49:07 +0800

    Add Control Panel
    Control Panel For Admin portals

---
+++ App/Controllers/ControlPanel.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use \App\Auth;
+use \App\Models\Announcement;
+use \App\Models\loan;
+use \App\Models\Contribution;
+use \App\Models\User;
+use \App\Models\Group;
+use \App\Models\Borrow;
+use \App\Models\Term;
+use \App\Flash;
+
+class ControlPanel extends Authenticated
+{
+    public function indexAction()
+    {
+        # code...
+        $user_info  = User::findByID($_SESSION['user_id']);
+    
+        View::renderTemplate('ControlPanel/index.html', [
+                            'user_info'         => $user_info
+                        ]);
+    }
+
+    /**
+     * Admin Members form index
+     *
+     * @return void
+     */
+    public function membersAction()
+    {
+        if (isset($this->route_params['id'])) {
+            # code...
+            $info           = User::searchById($this->route_params['id']);
+            $contri         = Contribution::records($this->route_params['id']);
+            $loan_records   = Loan::records($this->route_params['id']);
+
+            View::renderTemplate('Admin/member-info.html', [
+                'info' => $info,
+                'contri_records' => $contri,
+                'loan_records' => $loan_records
+            ]);
+
+        } else {
+            $members = User::getMembers();
+        
+            $groups = Group::getGroups();
+
+            $accesses = User::getAccessRights();
+
+            View::renderTemplate('Admin/members.html', [
+            'members' => $members,
+            'groups' => $groups,
+            'accesses' => $accesses
+        ]);
+
+        }
+    }
+
+    /**
+     * Admin Group form index
+     *
+     * @return void
+     */
+    public function groupsAction()
+    {
+        $groups = Group::getGroups();
+
+        View::renderTemplate('Admin/groups.html', [
+            'groups' => $groups
+        ]);
+
+    }
+
+
+    /**
+     * Admin Access form index
+     *
+     * @return void
+     */
+    public function accessAction()
+    {
+        $accesses = User::getAccessRights();
+
+        View::renderTemplate('Admin/access.html', [
+            'accesses' => $accesses
+        ]);
+
+    }
+
+    public function termsAction()
+    {
+        $terms = new Term();
+
+        $terms_list = $terms->view();
+
+        View::renderTemplate('/ControlPanel/term.html',[
+            'terms' => $terms_list
+        ]);
+    }
+}
